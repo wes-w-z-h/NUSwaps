a332ca4624db1630748ac7f6c55ab67cc5d6883c
Zac <zacheryngky@gmail.com>
2024-05-24 11:20:32 +0800

    close #2 close #3; Validate user requests during create and update

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -4,8 +4,6 @@ import UserModel from '../models/userModel.js';
 
 export const getUsers: RequestHandler = async (_req, res, next) => {
   try {
-    // throw Error("pewpew");
-    // remove the password field form the response
     const data = await UserModel.find({});
     res.status(200).json(data.map((user) => user.createResponse()));
   } catch (error) {
@@ -17,7 +15,11 @@ export const getUser: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
   try {
     const data = await UserModel.findById(id);
-    res.status(200).json(data?.createResponse());
+    if (data) {
+      res.status(200).json(data.createResponse());
+    } else {
+      res.status(404).json({ msg: 'User not found' });
+    }
   } catch (error) {
     next(error);
   }
@@ -27,7 +29,11 @@ export const deleteUser: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
   try {
     const data = await UserModel.findByIdAndDelete(id);
-    res.status(200).json(data?.createResponse());
+    if (data) {
+      res.status(200).json(data.createResponse());
+    } else {
+      res.status(404).json({ msg: 'User not found' });
+    }
   } catch (error) {
     next(error);
   }
@@ -37,13 +43,63 @@ export const updateUser: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
 
   try {
-    const data = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
-    res.status(200).json(data?.createResponse());
+    const data = await UserModel.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    if (data) {
+      res.status(200).json(data.createResponse());
+    } else {
+      res.status(404).json({ msg: 'User not found' });
+    }
   } catch (error) {
     next(error);
   }
 };
 
+export const changePassword: RequestHandler = async (req, res, next) => {
+  const { id } = req.params;
+  if (!('password' in req.body)) {
+    res.status(400).json({ msg: 'New password not specified' });
+  }
+  const update = { password: req.body.password };
+  try {
+    const data = await UserModel.findByIdAndUpdate(id, update, {
+      runValidators: true,
+      new: true,
+    });
+    if (data) {
+      res.status(200).json(data.createResponse());
+    } else {
+      res.status(404).json({ msg: 'User not found ' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const changeUsername: RequestHandler = async (req, res, next) => {
+  const { id } = req.params;
+  if (!('username' in req.body)) {
+    res.status(400).json({ msg: 'New username not specified' });
+  }
+  const update = { username: req.body.username };
+  try {
+    const data = await UserModel.findByIdAndUpdate(id, update, {
+      runValidators: true,
+      new: true,
+    });
+    if (data) {
+      res.status(200).json(data.createResponse());
+    } else {
+      res.status(404).json({ msg: 'User not found ' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// FIXME: Create defualt user with no swaps
 export const createUser: RequestHandler = async (req, res, next) => {
   try {
     const data = await UserModel.create(req.body);
