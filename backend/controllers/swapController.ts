81371fb266e920450a919033fd36af0536d13d64
Zac <zacheryngky@gmail.com>
2024-05-28 00:21:57 +0800

    Standardize code for User and Swap controller

--- backend/controllers/swapController.ts
+++ backend/controllers/swapController.ts
@@ -4,11 +4,9 @@ import { SwapModel } from '../models/swapModel.js';
 export const getSwaps: RequestHandler = async (req, res, next) => {
   await SwapModel.find({})
     .exec()
-    .then((data) =>
-      data
-        ? res.status(200).json(data.map((d) => d.createResponse()))
-        : res.status(404).json({ msg: 'No swaps.' })
-    )
+    .then((data) => {
+      res.status(200).json(data.map((swap) => swap.createResponse()));
+    })
     .catch((error) => next(error));
 };
 
@@ -19,71 +17,52 @@ export const getSwap: RequestHandler = async (req, res, next) => {
     .then((data) =>
       data
         ? res.status(200).json(data.createResponse())
-        : res.status(404).json({ msg: 'Swap not found.' })
+        : res.status(404).json({ msg: 'Swap not found' })
     )
     .catch((error) => next(error));
 };
 
 export const deleteSwap: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
-
   await SwapModel.findByIdAndDelete(id)
     .exec()
     .then((data) =>
       data
         ? res.status(200).json(data.createResponse())
-        : res.status(404).json({ msg: 'Swap not found.' })
+        : res.status(404).json({ msg: 'Swap not found' })
     )
     .catch((error) => next(error));
 };
 
 export const updateSwap: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
-  const { userId, courseId, lessonType, current, request, status } = req.body;
-  if (current.lessonType === request.lessonType) {
-    await SwapModel.findByIdAndUpdate(
-      id,
-      {
-        userId,
-        courseId,
-        lessonType,
-        current,
-        request,
-        status,
-      },
-      { new: true }
-    )
-      .exec()
-      .then((data) =>
-        data
-          ? res.status(200).json(data.createResponse())
-          : res.status(404).json({ msg: 'Swap not found.' })
-      )
-      .catch((error) => next(error));
-  } else {
-    res.status(400).json({ error: 'Incompatible lessons to be swapped.' });
+  if (req.body.current.lessonType !== req.body.request.lessonType) {
+    res.status(400).json({ error: 'Incompatible lessons to be swapped' });
   }
+
+  await SwapModel.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  })
+    .exec()
+    .then((data) =>
+      data
+        ? res.status(200).json(data.createResponse())
+        : res.status(404).json({ msg: 'Swap not found' })
+    )
+    .catch((error) => next(error));
 };
 
 export const createSwap: RequestHandler = async (req, res, next) => {
-  const { userId, courseId, lessonType, current, request, status } = req.body;
-
-  if (current.lessonType === request.lessonType) {
-    await SwapModel.create({
-      userId,
-      courseId,
-      lessonType,
-      current,
-      request,
-      status,
-    })
-      .then((resp) =>
-        resp
-          ? res.status(201).json(resp.createResponse())
-          : res.status(404).json({ msg: 'Swap not found.' })
-      )
-      .catch((error) => next(error));
-  } else {
-    res.status(400).json({ error: 'Incompatible lessons to be swapped.' });
+  if (req.body.current.lessonType !== req.body.request.lessonType) {
+    res.status(400).json({ error: 'Incompatible lessons to be swapped' });
   }
+
+  await SwapModel.create(req.body)
+    .then((data) =>
+      data
+        ? res.status(201).json(data.createResponse())
+        : res.status(400).json({ msg: 'Cannot create swap' })
+    )
+    .catch((error) => next(error));
 };
--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -3,58 +3,51 @@ import { RequestHandler } from 'express';
 import UserModel from '../models/userModel.js';
 
 export const getUsers: RequestHandler = async (_req, res, next) => {
-  try {
-    const data = await UserModel.find({});
-    res.status(200).json(data.map((user) => user.createResponse()));
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.find({})
+    .exec()
+    .then((data) => {
+      res.status(200).json(data.map((user) => user.createResponse()));
+    })
+    .catch((error) => next(error));
 };
 
 export const getUser: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
-  try {
-    const data = await UserModel.findById(id);
-    if (data) {
-      res.status(200).json(data.createResponse());
-    } else {
-      res.status(404).json({ msg: 'User not found' });
-    }
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.findById(id)
+    .exec()
+    .then((data) =>
+      data
+        ? res.status(200).json(data.createResponse())
+        : res.status(404).json({ msg: 'User not found' })
+    )
+    .catch((error) => next(error));
 };
 
 export const deleteUser: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
-  try {
-    const data = await UserModel.findByIdAndDelete(id);
-    if (data) {
-      res.status(200).json(data.createResponse());
-    } else {
-      res.status(404).json({ msg: 'User not found' });
-    }
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.findByIdAndDelete(id)
+    .exec()
+    .then((data) =>
+      data
+        ? res.status(200).json(data.createResponse())
+        : res.status(404).json({ msg: 'User not found' })
+    )
+    .catch((error) => next(error));
 };
 
 export const updateUser: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
-
-  try {
-    const data = await UserModel.findByIdAndUpdate(id, req.body, {
-      runValidators: true,
-      new: true,
-    });
-    if (data) {
-      res.status(200).json(data.createResponse());
-    } else {
-      res.status(404).json({ msg: 'User not found' });
-    }
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  })
+    .exec()
+    .then((data) =>
+      data
+        ? res.status(200).json(data.createResponse())
+        : res.status(404).json({ msg: 'User not found' })
+    )
+    .catch((error) => next(error));
 };
 
 export const changePassword: RequestHandler = async (req, res, next) => {
@@ -62,20 +55,19 @@ export const changePassword: RequestHandler = async (req, res, next) => {
   if (!('password' in req.body)) {
     res.status(400).json({ msg: 'New password not specified' });
   }
+
   const update = { password: req.body.password };
-  try {
-    const data = await UserModel.findByIdAndUpdate(id, update, {
-      runValidators: true,
-      new: true,
-    });
-    if (data) {
-      res.status(200).json(data.createResponse());
-    } else {
-      res.status(404).json({ msg: 'User not found ' });
-    }
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.findByIdAndUpdate(id, update, {
+    runValidators: true,
+    new: true,
+  })
+    .exec()
+    .then((data) =>
+      data
+        ? res.status(200).json(data.createResponse())
+        : res.status(404).json({ msg: 'User not found' })
+    )
+    .catch((error) => next(error));
 };
 
 export const changeUsername: RequestHandler = async (req, res, next) => {
@@ -83,28 +75,28 @@ export const changeUsername: RequestHandler = async (req, res, next) => {
   if (!('username' in req.body)) {
     res.status(400).json({ msg: 'New username not specified' });
   }
+
   const update = { username: req.body.username };
-  try {
-    const data = await UserModel.findByIdAndUpdate(id, update, {
-      runValidators: true,
-      new: true,
-    });
-    if (data) {
-      res.status(200).json(data.createResponse());
-    } else {
-      res.status(404).json({ msg: 'User not found ' });
-    }
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.findByIdAndUpdate(id, update, {
+    runValidators: true,
+    new: true,
+  })
+    .exec()
+    .then((data) =>
+      data
+        ? res.status(200).json(data.createResponse())
+        : res.status(404).json({ msg: 'User not found' })
+    )
+    .catch((error) => next(error));
 };
 
 // FIXME: Create defualt user with no swaps
 export const createUser: RequestHandler = async (req, res, next) => {
-  try {
-    const data = await UserModel.create(req.body);
-    res.status(201).json(data.createResponse());
-  } catch (error) {
-    next(error);
-  }
+  await UserModel.create(req.body)
+    .then((data) =>
+      data
+        ? res.status(201).json(data.createResponse())
+        : res.status(400).json({ msg: 'Cannot create user' })
+    )
+    .catch((error) => next(error));
 };
