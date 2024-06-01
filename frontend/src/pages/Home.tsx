5dd40a3aa67206750903284911d5922bfef11b2c
Zac <zacheryngky@gmail.com>
2024-06-02 00:05:03 +0800

    Rename views to pages

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar.tsx';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home">
+        <h2>Homepage</h2>
+      </div>
+    </>
+  );
+};
+
+export default Home;
