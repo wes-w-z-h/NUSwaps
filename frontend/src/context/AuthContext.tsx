9699b062176ee9546f51e6dfdc72caa9e032f033
wesley <wesleywong08@gmail.com>
2024-06-02 01:55:46 +0800

    Fix type issues

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,24 +1,40 @@
-import { createContext, useReducer } from 'react';
+import { Dispatch, ReactNode, createContext, useReducer } from 'react';
+import { User } from '../types/User';
 
-export const AuthContext = createContext();
+type AuthState = {
+  user: User | null;
+};
 
-export const authReducer = (state, action) {
-    switch (action.type) {
-        case 'LOGIN':
-            return { user: action.payload };
-        case 'LOGOUT':
-            return { user: null };
-        default:
-            return state;
-    }
-}
+type AuthAction = { type: 'LOGIN'; payload: User } | { type: 'LOGOUT' };
 
-export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, {
-        user: null,
-    });
-    
-    return <AuthContext.Provider value={{ ...state, dispatch }}>
-        { children }
+type AuthContextType = { state: AuthState; dispatch: Dispatch<AuthAction> };
+
+export const AuthContext = createContext<AuthContextType>(
+  {} as AuthContextType
+);
+
+// FIXME: Fix the fast refresh issue, extract it if possible
+export const authReducer = (state: AuthState, action: AuthAction) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { user: action.payload };
+    case 'LOGOUT':
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
     </AuthContext.Provider>
-}
+  );
+};
