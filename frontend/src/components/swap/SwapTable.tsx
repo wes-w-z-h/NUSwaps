2379cc52123dba5eaf0f1e7da94edf0f3380b5f1
wesley <wesleywong08@gmail.com>
2024-06-12 02:20:35 +0800

    Update error handling

--- frontend/src/components/swap/SwapTable.tsx
+++ frontend/src/components/swap/SwapTable.tsx
@@ -5,12 +5,12 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import { Swap } from '../../types/Swap';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import SwapRow from './SwapRow';
+import Alert from '@mui/material/Alert';
 
 const SwapTable = () => {
   const [swaps, setSwaps] = useState<Swap[]>([]);
@@ -19,18 +19,21 @@ const SwapTable = () => {
 
   useEffect(() => {
     const getSwaps = async () => {
-      try {
-        await axios
-          .get<Swap[]>('http://localhost:4000/api/swaps/userswaps', {
-            headers: { Authorization: `Bearer ${state.user?.token}` },
-          })
-          .then((data) => {
-            setSwaps(data.data);
-            console.log(data.data);
-          });
-      } catch (err) {
-        setError('Failed to fetch swaps');
-      }
+      await axios
+        .get<Swap[]>('http://localhost:4000/api/swaps/userswaps', {
+          headers: { Authorization: `Bearer ${state.user?.token}` },
+        })
+        .then((data) => {
+          setSwaps(data.data);
+          console.log(data.data);
+        })
+        .catch((error: AxiosError<{ error: string }>) => {
+          console.log(error);
+          const message = error.response?.data
+            ? `, ${error.response.data.error}`
+            : '';
+          setError(error.toString() + message);
+        });
     };
 
     if (state.user) {
@@ -38,29 +41,29 @@ const SwapTable = () => {
     }
   }, [state.user]);
 
-  if (error) {
-    return <Typography>{error}</Typography>;
-  }
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible swaps table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Course ID</TableCell>
-            <TableCell>Lesson Type</TableCell>
-            <TableCell>Current</TableCell>
-            <TableCell>Request</TableCell>
-            <TableCell>Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {swaps.map((swap) => (
-            <SwapRow key={swap.id} row={swap} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {error && <Alert severity="error">{error}</Alert>}
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible swaps table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Course ID</TableCell>
+              <TableCell>Lesson Type</TableCell>
+              <TableCell>Current</TableCell>
+              <TableCell>Request</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {swaps.map((swap) => (
+              <SwapRow key={swap.id} row={swap} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
