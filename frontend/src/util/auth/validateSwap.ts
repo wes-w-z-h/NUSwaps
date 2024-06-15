c8247faba3f2a8d8565811fbe06da72b25e137c1
wesley <wesleywong08@gmail.com>
2024-06-16 07:07:39 +0800

    Update validation for swap

---
+++ frontend/src/util/auth/validateSwap.ts
@@ -0,0 +1,33 @@
+const validateSwap = (
+  courseId: string,
+  lessonType: string,
+  current: string,
+  request: string
+) => {
+  // Initialise object to track errors
+  const inputErrors = {
+    courseId: '',
+    lessonType: '',
+    current: '',
+    request: '',
+  };
+
+  // Check empty email or password
+  if (!courseId) {
+    inputErrors.courseId = 'CourseId should not be empty';
+  }
+  if (!lessonType) {
+    inputErrors.lessonType = 'LessonType should not be empty';
+  }
+  if (!current) {
+    inputErrors.current = 'Current should not be empty';
+  }
+
+  if (!request) {
+    inputErrors.request = 'Request should not be empty';
+  }
+
+  return inputErrors;
+};
+
+export default validateSwap;
