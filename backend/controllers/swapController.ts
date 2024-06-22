062d819395aa0913c933f9c4d6b85f46c41edea2
wesley <wesleywong08@gmail.com>
2024-06-22 23:58:36 +0800

    Add new validator in controller

--- backend/controllers/swapController.ts
+++ backend/controllers/swapController.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import createHttpError from 'http-errors';
+import createHttpError, { isHttpError } from 'http-errors';
 import { SwapModel } from '../models/swapModel.js';
 
 export const getSwaps: RequestHandler = async (req, res, next) => {
@@ -67,17 +67,61 @@ export const updateSwap: RequestHandler = async (req, res, next) => {
 };
 
 export const createSwap: RequestHandler = async (req, res, next) => {
-  if (!req.body.current || !req.body.request) {
-    res.status(400).json({ error: 'Incomplete request body' });
-  } else if (req.body.current.lessonType !== req.body.request.lessonType) {
-    res.status(400).json({ error: 'Incompatible lessons to be swapped' });
-  } else {
-    await SwapModel.create({ userId: req.userId, ...req.body })
-      .then((data) =>
-        data
-          ? res.status(201).json(data.createResponse())
-          : res.status(400).json({ msg: 'Cannot create swap' })
-      )
-      .catch((error) => next(createHttpError(400, error.message)));
+  try {
+    if (!req.body.current || !req.body.request || !req.body.courseId) {
+      throw createHttpError(400, 'Incomplete request body');
+    }
+    const { current, request, courseId, lessonType } = req.body;
+    const { userId } = req;
+    if (current.lessonType !== request.lessonType) {
+      throw createHttpError(400, 'Incompatible lessons to be swapped');
+    }
+
+    let existing = await SwapModel.findOne({
+      userId,
+      courseId,
+      lessonType,
+    });
+
+    if (existing && !existing.current.endsWith(current.classNo)) {
+      throw createHttpError(
+        400,
+        `Invalid current slot, differs from existing swap`
+      );
+    }
+
+    existing = await SwapModel.findOne({
+      userId,
+      courseId,
+      lessonType,
+      current,
+      request,
+    });
+
+    if (existing) {
+      throw createHttpError(400, 'Duplicate swaps not allowed');
+    }
+
+    const data = await SwapModel.create({
+      userId,
+      courseId,
+      lessonType,
+      current,
+      request,
+    });
+
+    if (!data) {
+      throw createHttpError(404, 'User not found');
+    }
+
+    res.status(200).json(data.createResponse());
+  } catch (error: unknown) {
+    // TODO: see if possible to do this better
+    if (isHttpError(error)) {
+      next(error);
+    } else if (error instanceof Error) {
+      next(createHttpError(400, error.message));
+    }
+    next(error);
   }
 };
