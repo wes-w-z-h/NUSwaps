cad2d6c36035250ea5c3eddd186aeaf0359569e8
wesley <wesleywong08@gmail.com>
2024-06-29 04:59:34 +0800

    Update static filepath

--- backend/app.ts
+++ backend/app.ts
@@ -34,7 +34,7 @@ app.use('/api/swaps', swapRouter);
 app.use('/api/matches', matchRouter);
 
 // use frontend routes
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 console.log(__dirname);
 console.log(__dirname.substring(0, __dirname.length - 8));
 const p = path.resolve(__dirname, '../../frontend/dist/', 'index.html');
