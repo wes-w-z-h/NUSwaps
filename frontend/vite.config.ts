f128c72d0966a1e85ca0369e6e3fb993eff67c09
wesley <wesleywong08@gmail.com>
2024-07-08 01:31:29 +0800

    Update vite dev config for docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target:
+            mode === 'docker'
+              ? 'http://server:4000/api'
+              : 'http://localhost:4000/api',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
+      port: 3000,
     },
-    port: 3000,
-  },
+  };
 });
