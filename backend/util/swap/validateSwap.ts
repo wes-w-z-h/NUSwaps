fa15bcda4eb0764cdb4edc712d058544e2a3d710
wesley <wesleywong08@gmail.com>
2024-07-14 03:41:19 +0800

    Revert "Add validation for backend against NUSMods api"
    This reverts commit fb40591b67fab73d55eb529607203b36ee02b8c0.

--- backend/util/swap/validateSwap.ts
+++ backend/util/swap/validateSwap.ts
@@ -2,56 +2,6 @@ import createHttpError from 'http-errors';
 import { SwapModel } from '../../models/swapModel.js';
 import { MatchModel } from '../../models/matchModel.js';
 import { SwapStatus } from '../../types/api.js';
-import { Module } from '../../types/modules.js';
-
-const isInNUSMods = async (
-  courseId: string,
-  lessonType: string,
-  current: string,
-  request: string
-): Promise<void> => {
-  // TODO: change to env vars
-  const ACAD_YEAR = '2023-2024';
-  const resp = await fetch(
-    `https://api.nusmods.com/v2/${ACAD_YEAR}/modules/${courseId.toUpperCase()}.json`
-  );
-
-  // courseId doesnt exist in NUSMods api
-  if (!resp.ok) {
-    throw createHttpError(404, 'Course does not exist');
-  }
-  const data: Module = await resp.json();
-
-  const lessons = data.semesterData[0].timetable;
-
-  let currExists = false;
-  let reqExists = false;
-  let ltExists = false;
-  for (let i = 0; i < lessons.length; i += 1) {
-    const rl = lessons[i];
-    if (!ltExists && rl.lessonType === lessonType) {
-      ltExists = true;
-    }
-    if (!currExists && rl.lessonType === lessonType && rl.classNo === current) {
-      currExists = true;
-    }
-    if (!reqExists && rl.lessonType === lessonType && rl.classNo === request) {
-      reqExists = true;
-    }
-  }
-
-  if (!ltExists) {
-    throw createHttpError(400, 'Invalid lesson type');
-  }
-
-  if (!currExists) {
-    throw createHttpError(400, 'Invalid lesson type + current slot');
-  }
-
-  if (!reqExists) {
-    throw createHttpError(400, 'Invalid lesson type + request slot');
-  }
-};
 
 // TODO: By right should throw a custom error if use this in the tele bot but laze
 export const validateSwap = async (
@@ -62,8 +12,6 @@ export const validateSwap = async (
   request: string,
   swapId?: string
 ) => {
-  await isInNUSMods(courseId, lessonType, current, request);
-
   const existing = await SwapModel.find({
     userId,
     courseId,
