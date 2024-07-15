2a76ae522b5b44805eb0f88a53fb6472d227f45e
wesley <wesleywong08@gmail.com>
2024-07-15 22:10:20 +0800

    Fix errro handling

--- backend/telebot/middleware/errorHandler.ts
+++ backend/telebot/middleware/errorHandler.ts
@@ -7,12 +7,22 @@ const errorHandler = async (ctx: CustomContext, next: NextFunction) => {
     await next();
   } catch (error) {
     if (isHttpError(error)) {
+      if (ctx.session.state === 0) {
+        await ctx.reply(
+          `Error occured (${error.statusCode}): ${error.name} - ${error.message}`
+        );
+        return;
+      }
       await ctx.editMessageText(
         `Error occured (${error.statusCode}): ${error.name} - ${error.message}`
       );
     } else if (error instanceof Error) {
+      if (ctx.session.state === 0) {
+        await ctx.reply(`Error occured: ${error.name} - ${error.message}`);
+        return;
+      }
       await ctx.editMessageText(
-        `Error occures: ${error.name} - ${error.message}`
+        `Error occured: ${error.name} - ${error.message}`
       );
     } else {
       await ctx.editMessageText('Unknown error occured!');
